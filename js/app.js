4d3bd61ba606665c83ea73e92b89793dc52d2994
nazar marchuk <marcus@marcus-Aspire-5536.(none)>
2014-03-17 08:44:34 +0200

    2

--- js/app.js
+++ js/app.js
@@ -9,7 +9,20 @@ addrBook.config(['$translateProvider',function($translateProvider) {
     SURNAME:'Surname',
     PHONE:'Phone',
     GROUP:'Group',
-    EDIT:'Edit'
+    EDIT:'Edit',
+    CONTACT_LIST_IS_EMPTY:'Your contact list is empty.',
+    SAVE:'Save',
+    CLOSE:'Close',
+    CANCEL:'Cancel',
+    SAMPLE_NAME:'John',
+    SAMPLE_SURNAME:'Doe',
+    SAMPLE_PHONE:'555222333',
+    SAMPLE_GROUP:'friends',
+    FILTER_:'Filter ...',
+    TOTAL:'Total',
+    REQUIRED:'Required',
+    NAME_FIELD_IS_REQUIRED:'\'Name\' field is required.',
+    PHONE_FIEILD_IS_REQUIRED_AND_NUMBER:'\'Phone\' field is required and should contain digits only.'
   })
   .translations('ua', {
     CONTACTS: 'Контакти',
@@ -19,7 +32,20 @@ addrBook.config(['$translateProvider',function($translateProvider) {
     SURNAME:'Прізвище',
     PHONE:'Телефон',
     GROUP:'Група',
-    EDIT:'Редагувати'
+    EDIT:'Редагувати',
+    CONTACT_LIST_IS_EMPTY:'Ваш список контактів пустий.',
+    SAVE:'Зберегти',
+    CLOSE:'Закрити',
+    CANCEL:'Відмінити',
+    SAMPLE_NAME:'Іван',
+    SAMPLE_SURNAME:'Іванів',
+    SAMPLE_PHONE:'555222333',
+    SAMPLE_GROUP:'друзі',
+    FILTER_:'Фільтр ...',
+    TOTAL:'Всього',
+    REQUIRED:'Обов\'язково',
+    NAME_FIELD_IS_REQUIRED:'\'Ім\'я\' є обов\'язковим.',
+    PHONE_FIEILD_IS_REQUIRED_AND_NUMBER:'\'Телефон\' є обов\'язковим і повинно містити тільки цифри.'
   });
   $translateProvider.preferredLanguage('en');
 }])
@@ -33,6 +59,7 @@ addrBook.config(['$translateProvider',function($translateProvider) {
       group:''
     }
   };
+  var contacts = []; //saved contacts
   return {
     getCurrentContact: function(){
       return currentContact;
@@ -41,7 +68,12 @@ addrBook.config(['$translateProvider',function($translateProvider) {
       if(!arguments.length || !cObj){
         return this.resetCurrentContact();
       }
-      currentContact = cObj;
+      //currentContact = cObj; //changes saved by angular databinding,changes not reverted on form close
+      currentContact.id = cObj.id;
+      currentContact.data.name = cObj.data.name;
+      currentContact.data.surname = cObj.data.surname;
+      currentContact.data.phone = cObj.data.phone;
+      currentContact.data.group = cObj.data.group;
       return this;
     },
     resetCurrentContact: function(){
@@ -55,31 +87,59 @@ addrBook.config(['$translateProvider',function($translateProvider) {
         }
       };
       return this;
+    },
+    load: function(){ //load from storage
+      
+      contacts = [];
+      return contacts;
+    },
+    synch:function(contactsList){ // sycnh with storage
+      contacts = contactsList;
     }
   }
 }])
 .controller('MainCtrl',['$scope','$translate','Contact',function($scope,$translate,Contact){
-  $scope.contacts = [];
-  $scope.contacts.push({
-    id:1,
-    data:{
-      name:"naz",
-      surname:"mar",
-      phone:"12312",
-      group:"test"
-    }
-  });
+  $scope.contacts = Contact.load();
+  
+  $scope.langs = ['en','ua'];
+  $scope.isLangActive = function(code){
+    return code == $translate.use();
+  }
   $scope.setLang = function(code){
     // console.log($translate.use());
-    $translate.use(code);
+    if($scope.langs.indexOf(code) != -1){
+      $translate.use(code);
+    }
   };
   $scope.getUserFormLabel = function(){
     return 'ADD_NEW_CONTACT';
   };
+  $scope.ordAsc = 0;
+  $scope.formErrors = [];
+  $scope.changeOrder = function(){
+    $scope.ordAsc = !$scope.ordAsc;
+  }
+  $scope.validateContactData = function(cObj){
+    var err = [];
+    if( !cObj.name ){
+      err.push('NAME_FIELD_IS_REQUIRED');
+    }
+    if( !cObj.phone || isNaN(cObj.phone) ){
+      err.push('PHONE_FIEILD_IS_REQUIRED_AND_NUMBER');
+    }
+    return err;
+  }
   $scope.saveContact = function(){
     console.log('TO SAVE:',$scope.currCont.id,$scope.currCont.data);
     var c_id = $scope.currCont.id || (1*$scope.contacts.length + 1);
     var c_data = $scope.currCont.data;
+    var fErr = $scope.validateContactData(c_data);
+    if( fErr.length ){
+      $scope.formErrors = fErr;
+      return false;
+    }else{
+      $scope.formErrors = [];
+    }
     var newCont = $scope.contacts.filter(function(c){
       if(c.id == c_id){
         c.data = c_data;
@@ -91,14 +151,21 @@ addrBook.config(['$translateProvider',function($translateProvider) {
     if(!newCont[0]){
       $scope.contacts.push({id:c_id,data:c_data});
     }
+    Contact.resetCurrentContact();
+    Contact.synch($scope.contacts);
     angular.element('#contact_form').modal('hide');
   };
+  $scope.closeCF = function(){
+    console.debug(Contact.getCurrentContact());
+    $scope.currCont = Contact.getCurrentContact();
+  }
   $scope.findById = function(id){
     var c = $scope.contacts.filter(function(c){return c.id == id;});
     return c[0];
   }
   $scope.prepareContactForm = function(id){
     var c = null;
+    $scope.formErrors = [];
     if( id ){ // edit contact
       c = $scope.findById(id);
     }
